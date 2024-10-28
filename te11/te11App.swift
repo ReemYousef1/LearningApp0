f97effa8ca2ead21b4fef4a27ea9b2f1f8d30920
ReemYousef1 <109497565+ReemYousef1@users.noreply.github.com>
2024-10-28 18:39:11 +0300

    Initial Commit

---
+++ te11/te11App.swift
@@ -0,0 +1,17 @@
+//
+//  te11App.swift
+//  te11
+//
+//  Created by Reem on 25/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct te11App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
